45af7a711dd6a097e4dfe20036cc9e3dcbc7c69e
keeper034 <107176421+keeper034@users.noreply.github.com>
2022-06-09 15:26:19 +0300

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,8 +1,18 @@
-a = int(input('Введите число а '))
-b = int(input('Введите число b '))
+a = input("Введите первое число: ")
+if(a.isdigit()):
+    a = int(a)
+    print("Вы ввели первую цифру " , a)
+else:
+   print("Вы ввели строку или ничего не ввели ")
+
+b = input("Введите второе число: ")
+if(b.isdigit()):
+    b = int(b)
+    print("Вы ввели вторую цифру " , b)
+else:
+   print("Вы ввели строку или ничего не ввели")
 
 def compare (a,b):
-
     x = True
     y = False
     if a > b:
